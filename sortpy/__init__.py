4c86cf4a6e05d88647ac28ed36655f4b4c5b6616
Denis Anikin <ad@xfenix.ru>
2020-08-03 02:51:33 +0300

    Description updated + version bump

--- sortpy/__init__.py
+++ sortpy/__init__.py
@@ -1,6 +1,6 @@
-"""Core package module.
+"""This module contains some popular sorting algorithms implemented in pure python.
 """
 
 
-VERSION: tuple = (1, 0, 7)
+VERSION: tuple = (1, 0, 8)
 __version__: str = ".".join(map(str, VERSION))
